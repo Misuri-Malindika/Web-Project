b24d6b15ee4035256ae22cb4c6dd384658687a9c
Misuri M Wijewardhana <misuri.malindika@gmail.com>
2023-08-20 12:20:06 +0530

    Update submitapplication.php

--- Pick your Pet/PHP/submitapplication.php
+++ Pick your Pet/PHP/submitapplication.php
@@ -14,13 +14,11 @@ $conn = new mysqli('localhost', 'id21085883_root', 'Petshop@2023', 'id21085883_p
 if ($conn->connect_error){
     die("connection failed: " .$conn->connect_error);
 } else{
-    //$stmt = $conn->prepare("insert into register(Fname, Email, Phone, Address, City, Petcode, Experience)
-    //values(?, ?.?, ?, ?, ?, ?)");
     $stmt = $conn->prepare("insert into adoption(FName, Email, Phone, Address, City, PetCode, Experience)
     VALUES (?, ?, ?, ?, ?, ?, ?)");
     $stmt->bind_param("sssssss", $FName, $Email, $Phone, $Address, $City, $PetCode, $Experience);
     $stmt->execute();
-    echo "Application Submitted Successfully!";
+    echo "Registration Successfully";
     $stmt->close();
     $conn->close();
 }
